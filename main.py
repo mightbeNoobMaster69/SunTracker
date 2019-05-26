d959c1f8308d7fced0af512f478679624ea0d6fa
mightBeNoobMaster69 <lucas01martiniano@gmail.com>
2019-05-26 15:07:03 -0300

    c

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ with open('visual_processing/data/data.json') as f:
 
 data['sunx'] = 90 #modifies the sun x pos in json "sunx"
 data['suny'] = 90 #modifies the sun y pos in json "suny"
-data['angle'] = 0 #modifies the angle in json "angle"
+data['angle'] = 45 #modifies the angle in json "angle"
 
 #writes out changes to the json file
 with open('visual_processing/data/data.json', 'w') as f:
